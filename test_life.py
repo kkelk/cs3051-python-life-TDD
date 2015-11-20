d5eb7e58458008d8ef5404700bf76a189973b3e6
Kieran Kelk <kieran@kierankelk.co.uk>
2015-11-20 11:21:14 +0000

    Add test for setting certain cells to be living.

--- test_life.py
+++ test_life.py
@@ -8,3 +8,12 @@ def test_create_game():
 def test_check_initially_blank():
     game = Life()
     assert len(list(game.living)) == 0
+
+
+def test_set_living():
+    game = Life()
+    game.set_living((0, 0))
+    game.set_living((3, 3))
+
+    assert (0, 0) in game.living
+    assert (3, 3) in game.living
