0b5e5a1a90256d6391669f662d52d2b2df16de10
Kieran Kelk <kieran@kierankelk.co.uk>
2015-11-20 12:55:16 +0000

    flow: Created branch 'feature/over-population'.

---
+++ life/life.py
@@ -0,0 +1,57 @@
+class Life(object):
+    def __init__(self, living_set=None):
+        if living_set:
+            self._living = living_set
+        else:
+            self._living = set()
+
+    @property
+    def living(self):
+        for cell in self._living:
+            yield cell
+
+    def count_living_neighbours(self, (x, y)):
+        count = 0
+        for neighbour in self.neighbours((x, y)):
+            if self.is_alive(neighbour):
+                count += 1
+
+        return count
+
+    def is_alive(self, (x, y)):
+        return (x, y) in self._living
+
+    def neighbours(self, (x, y)):
+        if type(x) is not int or type(y) is not int:
+            raise TypeError('It is only possible to calculate the neighbours for an integer co-ordinate pair.')
+
+        yield (x - 1, y - 1)
+        yield (x - 1, y)
+        yield (x - 1, y + 1)
+        yield (x, y + 1)
+        yield (x + 1, y + 1)
+        yield (x + 1, y)
+        yield (x + 1, y - 1)
+        yield (x, y - 1)
+
+    def set_dead(self, (x, y)):
+        if type(x) is not int or type(y) is not int:
+            raise TypeError('It is only possible to set an integer co-ordinate pair to be a dead cell.')
+        self._living.discard((x, y))
+
+    def set_living(self, (x, y)):
+        if type(x) is not int or type(y) is not int:
+            raise TypeError('It is only possible to set an integer co-ordinate pair to be a living cell.')
+        self._living.add((x, y))
+
+    def tick(self):
+        # Take a copy of the current board state, since the next board state is a pure function of the current one, and should not be affected by intermediate results.
+        next_iteration = Life(living_set=self._living.copy())
+
+        # Underpopulation deaths.
+        for cell in self.living:
+            if self.count_living_neighbours(cell) < 2:
+                next_iteration.set_dead(cell)
+
+        # Once all modifications to the next board state have been calculated, replace the current board with the newly calculated one.
+        self._living = next_iteration._living
---
+++ test_life.py
@@ -0,0 +1,173 @@
+import pytest
+
+from life.life import Life
+
+
+def test_create_game():
+    assert Life()
+
+
+def test_check_initially_blank():
+    game = Life()
+    assert len(list(game.living)) == 0
+
+
+def test_set_living():
+    game = Life()
+    game.set_living((0, 0))
+    game.set_living((-1, -3))
+    game.set_living((3, 3))
+
+    assert (0, 0) in game.living
+    assert (-1, -3) in game.living
+    assert (3, 3) in game.living
+
+
+def test_non_integer_set_living():
+    game = Life()
+    with pytest.raises(TypeError):
+        game.set_living((True, True))
+    with pytest.raises(TypeError):
+        game.set_living(('test', 'test'))
+
+    assert (True, True) not in game.living
+    assert ('test', 'test') not in game.living
+
+
+def test_repeated_set_living():
+    game = Life()
+    assert (0, 0) not in game.living
+    game.set_living((0, 0))
+    assert (0, 0) in game.living
+
+    game.set_living((0, 0))
+    assert (0, 0) in game.living
+
+
+def test_is_alive():
+    game = Life()
+    assert not game.is_alive((0, 0))
+
+    game.set_living((0, 0))
+    assert game.is_alive((0, 0))
+
+
+def test_set_dead():
+    game = Life()
+    game.set_dead((0, 0))
+    assert not game.is_alive((0, 0))
+
+    game.set_living((0, 0))
+    assert game.is_alive((0, 0))
+    game.set_dead((0, 0))
+    assert not game.is_alive((0, 0))
+
+
+def test_non_integer_set_dead():
+    # Technically nothing bad could happen by them calling set_dead with something invalid anyway --- since it could never exist in the set as set_living checks it is valid. It seems better to explicitly throw an exception, however, so that if an accidental mistake was made, it could be caught early.
+    game = Life()
+    with pytest.raises(TypeError):
+        game.set_dead((True, True))
+    with pytest.raises(TypeError):
+        game.set_dead(('test', 'test'))
+
+    assert (True, True) not in game.living
+    assert ('test', 'test') not in game.living
+
+
+def test_neighbours():
+    game = Life()
+    neighbours = (
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1)
+    )
+
+    assert len(neighbours) == len(list(game.neighbours((0, 0))))
+    assert sorted(neighbours) == sorted(list(game.neighbours((0, 0))))
+
+
+def test_invalid_neighbours():
+    game = Life()
+    with pytest.raises(TypeError):
+        list(game.neighbours((True, True)))
+
+
+def test_count_living_neighbours():
+    game = Life()
+    assert game.count_living_neighbours((0, 0)) == 0
+
+    game.set_living((0, 1))
+    assert game.count_living_neighbours((0, 0)) == 1
+
+    game.set_living((1, 1))
+    assert game.count_living_neighbours((0, 0)) == 2
+    assert game.count_living_neighbours((0, 1)) == 1
+    assert game.count_living_neighbours((1, 1)) == 1
+
+
+def test_empty_tick():
+    game = Life()
+    assert len(list(game.living)) == 0
+    game.tick()
+    assert len(list(game.living)) == 0
+    game.tick()
+    assert len(list(game.living)) == 0
+
+
+def test_stable_block_tick():
+    game = Life()
+    game.set_living((0, 0))
+    game.set_living((0, 1))
+    game.set_living((1, 0))
+    game.set_living((1, 1))
+
+    assert len(list(game.living)) == 4
+    game.tick()
+    assert len(list(game.living)) == 4
+    game.tick()
+    assert len(list(game.living)) == 4
+
+
+def test_under_population_death():
+    game = Life()
+    game.set_living((0, 0))
+    assert game.is_alive((0, 0))
+
+    game.tick()
+    assert len(list(game.living)) == 0
+    game.tick()
+    assert len(list(game.living)) == 0
+
+
+def test_under_population_death_one_neighbour():
+    game = Life()
+    game.set_living((0, 0))
+    game.set_living((0, 1))
+    assert len(list(game.living)) == 2
+
+    game.tick()
+    assert len(list(game.living)) == 0
+
+
+def test_isolated_under_population():
+    game = Life()
+    game.set_living((0, 0))
+    game.set_living((2, 2))
+    game.set_living((2, 3))
+    game.set_living((2, 4))
+
+    assert len(list(game.living)) == 4
+
+    game.tick()
+
+    # In this case, we need to check specific spaces, since the number alive will change once the reproduction rule is implemented
+    assert not game.is_alive((0, 0))
+    assert not game.is_alive((2, 2))
+    assert not game.is_alive((2, 4))
+    assert game.is_alive((2, 3))
