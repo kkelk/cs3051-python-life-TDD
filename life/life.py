90460ae1b9c00f6482acbe83da68b3d44a13de04
Kieran Kelk <kieran@kierankelk.co.uk>
2015-11-20 12:05:08 +0000

    Implement calculating neighbours for a cell.
    Turned on verbose output from pytest --- was useful for debugging this section, as it shows full diffs on lists that mismatch.

--- life/life.py
+++ life/life.py
@@ -10,6 +10,16 @@ class Life(object):
     def is_alive(self, (x, y)):
         return (x, y) in self._living
 
+    def neighbours(self, (x, y)):
+        yield (x - 1, y - 1)
+        yield (x - 1, y)
+        yield (x - 1, y + 1)
+        yield (x, y + 1)
+        yield (x + 1, y + 1)
+        yield (x + 1, y)
+        yield (x + 1, y - 1)
+        yield (x, y - 1)
+
     def set_dead(self, (x, y)):
         if type(x) is not int or type(y) is not int:
             raise TypeError('It is only possible to set an integer co-ordinate pair to be a dead cell.')
