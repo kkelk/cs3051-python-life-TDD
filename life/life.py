48cd773329473d1358fe9b067f03390cfc02c377
Kieran Kelk <kieran@kierankelk.co.uk>
2015-11-20 11:27:09 +0000

    Implement raising exception in set_living if types are invalid.

--- life/life.py
+++ life/life.py
@@ -8,4 +8,6 @@ class Life(object):
             yield cell
 
     def set_living(self, (x, y)):
+        if type(x) is not int or type(y) is not int:
+            raise TypeError('It is only possible to set an integer co-ordinate pair to be a living cell.')
         self._living.add((x, y))
